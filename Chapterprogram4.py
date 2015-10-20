04e69aeef75053aaa9d36ec8197d07589af4c08f
xaowoodenfish <liuxuan@xao.ac.cn>
2015-10-20 16:15:24 +0800

    the trend of American population
    dddd

--- Chapterprogram4.py
+++ Chapterprogram4.py
@@ -1,3 +1,22 @@
-__author__ = 'leoxuan'
-# the population of America
+# the trend population of America after 1991.the number is just to do exercise,it not the really population
 # time 10.20 2015
+def brith(second):
+    brithNumber = second /7
+    return brithNumber
+def death(second):
+    deathNumber = second /13
+    return  deathNumber
+def immigrant(second):
+    immigrantNumber = second /35
+    return immigrantNumber
+def timeTosec(year):
+    year = year -1991
+    seco = year*365*86400
+    return seco
+population = 307357870
+ye = input('input the year after 1991:')
+ti = timeTosec(ye)
+population +=brith(ti)+death(ti)+immigrant(ti)
+print 'the year of %d America population is %f'%(ye,population)
+
+
